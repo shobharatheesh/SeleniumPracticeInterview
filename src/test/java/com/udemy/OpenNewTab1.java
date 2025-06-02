690d2645898f03e985f5b5627681fdcc202318f7
Shobha Rao <sraork@gmail.com>
2025-06-02 16:19:08 +0530

    Selenium Practice -- 2 june

---
+++ src/test/java/com/udemy/OpenNewTab1.java
@@ -0,0 +1,31 @@
+package com.udemy;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class OpenNewTab1 {
+
+    //Opening a new tab is a common keyboard shortcut (Ctrl + T) on most browsers
+
+    public static void main(String[] args)
+    {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        Actions actions = new Actions(driver);
+
+        actions.keyDown(Keys.CONTROL).sendKeys("t").keyUp(Keys.CONTROL).perform();
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
