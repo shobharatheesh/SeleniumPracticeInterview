5fdcdd6a4e963a57b9a5f34059c303ef47c638f7
Shobha Rao <sraork@gmail.com>
2025-06-03 14:21:08 +0530

    Selenium Practice -- 3 june

---
+++ src/test/java/com/udemy/DoubleClickExample.java
@@ -0,0 +1,38 @@
+package com.udemy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DoubleClickExample {
+
+    public static void main(String[] args)
+    {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+
+        WebElement button = driver.findElement(By.xpath("//button[text()='Double-Click Me To See Alert']"));
+
+        Actions actions = new Actions(driver);
+
+        actions.doubleClick(button).perform();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //handle alert
+
+        driver.switchTo().alert().accept();
+
+        //driver.quit();
+
+
+    }
+}
