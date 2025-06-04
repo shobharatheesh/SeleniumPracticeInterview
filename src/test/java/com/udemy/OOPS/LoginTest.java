1dbf01208bd0bb702ed743719394c2ea5da1b26b
Shobha Rao <sraork@gmail.com>
2025-06-04 09:01:04 +0530

    Selenium Practice -- 4 june

--- src/test/java/com/udemy/OOPS/LoginTest.java
+++ src/test/java/com/udemy/OOPS/LoginTest.java
@@ -1,4 +1,22 @@
 package com.udemy.OOPS;
 
-public class LoginTest {
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest{
+
+    @Test
+
+    public void verifyLogin()
+    {
+
+      LoginPage loginpage  = new LoginPage(driver);
+      driver.get("https://rbc.com/login");
+
+      loginpage.loginToRBC("adminUser","adminPass");
+
+      //add assertions for successful login
+
+    }
+
+
 }
