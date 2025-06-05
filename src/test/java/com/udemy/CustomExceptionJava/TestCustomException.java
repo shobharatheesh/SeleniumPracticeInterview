e2f71c557940f3d9e7eca30e36fe70be3a4df1bf
Shobha Rao <sraork@gmail.com>
2025-06-05 18:33:30 +0530

    Selenium Practice -- 5 june

---
+++ src/test/java/com/udemy/CustomExceptionJava/TestCustomException.java
@@ -0,0 +1,24 @@
+package com.udemy.CustomExceptionJava;
+
+public class TestCustomException {
+
+    public static void validate(int age) throws MyCustomException {
+        if (age < 18) {
+            throw new MyCustomException("Age must be 18 or above");
+        }
+
+    }
+
+
+        public static void main(String[] args)
+        {
+            try {
+                validate(15);
+            } catch (MyCustomException e) {
+                System.out.println("Exception : " + e.getMessage());
+            }
+
+
+        }
+    }
+
