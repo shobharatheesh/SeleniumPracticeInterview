3ad5b99e37061a8af987fe50921b1670f3b3d178
Shobha Rao <sraork@gmail.com>
2025-06-19 17:27:38 +0530

    Selenium Practice -- 19 june

---
+++ src/test/java/com/udemy/Encapsulation/Employee.java
@@ -0,0 +1,20 @@
+package com.udemy.Encapsulation;
+
+public class Employee {
+
+    private String name; //private data
+
+    //getter method
+
+    public String getName()
+    {
+        return name;
+    }
+
+    //setter method
+
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
+}
---
+++ src/test/java/com/udemy/Encapsulation/Main.java
@@ -0,0 +1,14 @@
+package com.udemy.Encapsulation;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Employee emp = new Employee();
+
+        emp.setName("John"); //setting value through setter
+
+        System.out.println(emp.getName()); //getting vaue through getter
+
+    }
+}
