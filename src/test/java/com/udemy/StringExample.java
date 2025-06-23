510ea6b37deb84b5c6ef54062dea75f1f992d057
Shobha Rao <sraork@gmail.com>
2025-06-23 23:13:47 +0530

    Selenium Practice -- 23 june

---
+++ src/test/java/com/udemy/StringExample.java
@@ -0,0 +1,16 @@
+package com.udemy;
+
+public class StringExample {
+
+    public static void main(String[] args)
+    {
+
+        String str = "Hello";
+
+        str = str + " World"; //creates a new string object
+
+        System.out.println(str);
+
+
+    }
+}
