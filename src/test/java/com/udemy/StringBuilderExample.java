64b9fbd91068317336094971dfc5ba73ad522c53
Shobha Rao <sraork@gmail.com>
2025-06-24 08:53:12 +0530

    Selenium Practice -- 24 june

---
+++ src/test/java/com/udemy/StringBuilderExample.java
@@ -0,0 +1,15 @@
+package com.udemy;
+
+public class StringBuilderExample {
+
+    public static void main(String[] args)
+    {
+
+      StringBuilder sb = new StringBuilder("Hello");
+
+      sb.append(" World");
+
+      System.out.println(sb);
+
+    }
+}
