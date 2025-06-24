6d9e27fadc9ad4be8f078f6a7ae1bcbc8b1235c5
Shobha Rao <sraork@gmail.com>
2025-06-24 23:07:15 +0530

    Selenium Practice -- 24 june

--- src/test/java/com/udemy/JavaPrograms/StringBuilder_RemoveDuplicates.java
+++ src/test/java/com/udemy/JavaPrograms/StringBuilder_RemoveDuplicates.java
@@ -1,4 +1,29 @@
 package com.udemy.JavaPrograms;
 
 public class StringBuilder_RemoveDuplicates {
+
+    public static void main(String[] args)
+    {
+        String input ="Programming";
+
+        StringBuilder result = new StringBuilder();
+        //result.reverse();
+        // System.out.println(result);
+        //System.out.println(input.length());
+
+        for(int i=0 ;i<input.length();i++)
+        {
+            char ch = input.charAt(i);
+
+            if(result.indexOf(String.valueOf(ch)) == -1)
+            {
+                result.append(ch);
+            }
+
+
+        }
+
+        System.out.println("After removing duplicates: " +result.toString());
+
+    }
 }
